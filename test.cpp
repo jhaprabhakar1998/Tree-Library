89472dee02175c143cc30f0ff11b31ed47fba080
PraJha <abcpr108@gmail.com>
2019-01-24 13:58:51 +0530

    First demo test.cpp

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<unordered_map>
+#include<set>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define printa(arr,n) for(int ia=0;ia<n;ia++)cout<<arr[ia]<<" "
+#define inputa(arr,n) for(int ia=0;ia<n;ia++)cin>>arr[ia]
+
+
+int main(){
+	int x,y;
+	cin>>x>>y;
+	int hello=10;
+	cout<<(x+y)<<endl;
+}
